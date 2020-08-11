19f9bcf6c9607fb4ac3b197d68954b01973c9869
WillBlua <willblua@outlook.com>
2020-08-11 16:35:45 +0800

    创建项目

---
+++ src/test/java/com/willblua/BlogWillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willblua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
